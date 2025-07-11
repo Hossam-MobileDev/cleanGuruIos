17efe0178bcdc49c1265c605759aa39aa678a24b
test <test@192.168.1.15>
2025-07-11 17:38:15 +0300

    Initial commit of Clean Guru iOS

---
+++ GHGG/appOpenAdManager.swift
@@ -0,0 +1,18 @@
+//
+//  appOpenAdManager.swift
+//  GHGG
+//
+//  Created by test on 11/07/2025.
+//
+
+import SwiftUI
+
+struct appOpenAdManager: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    appOpenAdManager()
+}
